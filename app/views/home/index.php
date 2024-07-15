cffeba6bcc5d914f5752bd9519c8cbdf3ea19cd4
Diana C <165112039+diSimply@users.noreply.github.com>
2024-07-15 23:55:06 +0000

    Initial commit

---
+++ app/views/home/index.php
@@ -0,0 +1,11 @@
+<?php require_once 'app/views/templates/header.php' ?>
+<div class="container page">
+    <div class="page-header" id="banner">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>Welcome to Movie Reviews App</h1>
+            </div>
+        </div>
+    </div>
+    <?php require_once 'app/views/templates/footer.php' ?>
+</div>
---
+++ app/views/templates/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="icon" href="/favicon.png">
+        <link rel="stylesheet" type="text/css" href="/css/styles.css">
+        <title>COSC 4806</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="mobile-web-app-capable" content="yes">
+    </head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+    <body>
+      <div id="page-container">
+        <div id="content-wrap">  
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Movie Reviews App</a>
+      </div>
+    </nav>

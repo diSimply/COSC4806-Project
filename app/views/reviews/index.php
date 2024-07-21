f241c7648505d9219ff290566753fc92fa5a6a64
Diana C <165112039+diSimply@users.noreply.github.com>
2024-07-21 18:45:12 +0000

    Updated review page to display reviews for the movie and average rating

--- app/views/reviews/index.php
+++ app/views/reviews/index.php
@@ -16,78 +16,61 @@
           <?php endif; ?>
         </div>
         <!-- Details  -->
-        <div class="col">
-          <div>Title: <?php echo $movie['Title']; ?></div>
-          <div>Year: <?php echo $movie['Year']; ?></div>
-          <div>Rated: <?php echo $movie['Rated']; ?></div>
-          <div>Released: <?php echo $movie['Released']; ?></div>
-          <div>Runtime: <?php echo $movie['Runtime']; ?></div>
-          <div>Genre: <?php echo $movie['Genre']; ?></div>
-          <div>Director: <?php echo $movie['Director']; ?></div>
-          <div>Actors: <?php echo $movie['Actors']; ?></div>
-    
-          <div class="mt-3 big-stars">
-            <span class="fa fa-star star checked"></span>
-            <span class="fa fa-star star checked"></span>
-            <span class="fa fa-star star checked"></span>
-            <span class="fa fa-star star"></span>
-            <span class="fa fa-star star"></span>
-          </div>       
+        <div class="col d-flex flex-column justify-content-between">
+          <div>
+            <div>Title: <?php echo $movie['Title']; ?></div>
+            <div>Year: <?php echo $movie['Year']; ?></div>
+            <div>Rated: <?php echo $movie['Rated']; ?></div>
+            <div>Released: <?php echo $movie['Released']; ?></div>
+            <div>Runtime: <?php echo $movie['Runtime']; ?></div>
+            <div>Genre: <?php echo $movie['Genre']; ?></div>
+            <div>Director: <?php echo $movie['Director']; ?></div>
+            <div>Actors: <?php echo $movie['Actors']; ?></div>
+          </div>
+
+          <div>
+            <div>Average Rating</div>
+            <div class="big-stars" id="average-rating">
+              <?php for($i = 0; $i < $data['average_rating']; $i++): ?>
+                <span class="fa fa-star star checked"></span>
+              <? endfor ;?>
+              <?php for($i = 0; $i < 5 - $data['average_rating']; $i++): ?>
+                <span class="fa fa-star star"></span>
+              <? endfor ;?>
+            </div>
+          </div>
         </div>
       </div>
     </div>
     <!-- reviews  -->
     <div class="col-lg-4">
-      <div>All reviews: </div>
+      <h3 class="mt-4 mt-lg-0">All reviews: </h3>
       <!-- single review -->
-      <div class="card w-100 mb-2" style="width: 18rem;">
-        <div class="card-body">
-          <p class="card-text">Ant man is a fantastic movie with lots of comedy.</p>
-          <div class="d-flex justify-content-between">
-          <div class="small-stars">
-            <span class="fa fa-star star checked"></span>
-            <span class="fa fa-star star checked"></span>
-            <span class="fa fa-star star checked"></span>
-            <span class="fa fa-star star"></span>
-            <span class="fa fa-star star"></span>
+      <div class="reviews d-flex flex-column">
+        <?php if(count($data['reviews']) == 0): ?>
+          <div class="alert alert-secondary">No reviews yet.</div>
+        <? else: ?>
+          <?php foreach($data['reviews'] as $review): ?>
+          <div class="card w-100 mb-2" style="width: 18rem;">
+            <div class="card-body">
+              <p class="card-text"><?php echo $review['comment'] ?></p>
+              <div class="d-flex justify-content-between">
+              <div class="small-stars">
+                <?php for($i = 0; $i < $review['rating']; $i++): ?>
+                  <span class="fa fa-star star checked"></span>
+                <?php endfor; ?>
+                <?php for($i = 0; $i < 5 - $review['rating']; $i++): ?>
+                  <span class="fa fa-star star"></span>
+                <?php endfor; ?>
+              </div>
+              <div><?php echo $review['created_at']; ?></div>
+              </div>
+            </div>
           </div>
-          <div>July 17th, 2024</div>
-          </div>
-        </div>
-      </div>
-      <div class="card w-100 mb-2" style="width: 18rem;">
-        <div class="card-body">
-          <p class="card-text">Ant man is a fantastic movie with lots of comedy.</p>
-          <div class="d-flex justify-content-between">
-          <div class="small-stars">
-            <span class="fa fa-star star checked"></span>
-            <span class="fa fa-star star checked"></span>
-            <span class="fa fa-star star checked"></span>
-            <span class="fa fa-star star"></span>
-            <span class="fa fa-star star"></span>
-          </div>
-          <div>July 17th, 2024</div>
-          </div>
-        </div>
-      </div>
-      <div class="card w-100 mb-2" style="width: 18rem;">
-        <div class="card-body">
-          <p class="card-text">Ant man is a fantastic movie with lots of comedy.</p>
-          <div class="d-flex justify-content-between">
-          <div class="small-stars">
-            <span class="fa fa-star star checked"></span>
-            <span class="fa fa-star star checked"></span>
-            <span class="fa fa-star star checked"></span>
-            <span class="fa fa-star star"></span>
-            <span class="fa fa-star star"></span>
-          </div>
-          <div>July 17th, 2024</div>
-          </div>
-        </div>
+        <?php endforeach ;?>
+        <? endif; ?>
       </div>
     </div>
   </div>
-  
-
 </div>
 <?php require_once 'app/views/templates/footer.php' ?>

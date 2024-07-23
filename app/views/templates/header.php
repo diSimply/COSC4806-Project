be154679492c8de94e4937a414bd2ef468f9e37a
Diana C <165112039+diSimply@users.noreply.github.com>
2024-07-23 00:23:44 +0000

    Added home link to the nav bar

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -17,6 +17,13 @@
         <div id="content-wrap">  
           <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="/">Movie Reviews App</a>
-      </div>
+        <a class="navbar-brand" href="#">Movie Reviews App</a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/">Home</a>
+            </li>
+          </ul>
+        </div>
+      </div>       
     </nav>
